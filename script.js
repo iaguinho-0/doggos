5a5849d7ccccbef16ceb7565cf80dd35a31a5d31
Iuri Sant <iuri.s.goes@gmail.com>
2023-11-29 22:22:16 -0300

    Add: sidebar

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 $('.carousel').carousel()
+
+function showSideBar() {
+  let statusbar = document.getElementsByClassName("side-bar");
+  console.log(statusbar)
+  if (statusbar[0].className.valueOf("show-sidebar")) {
+    statusbar[0].className = statusbar[0].className.replace("", "show-sidebar ")
+    console.log("On")
+  } else {
+    statusbar[0].className = statusbar[0].className.replace("show-sidebar", "")
+    console.log("Off")
+  }
+}
